7e211a51bff6ad37b397aa609dd5c11e29b1a4eb
Lauri Seppäläinen <lauri.seppalainen@helsinki.fi>
2024-02-19 09:46:33 +0200

    documentation; add spam dataset acquisition

--- experiments/data.py
+++ experiments/data.py
@@ -4,6 +4,7 @@ from typing import Union
 
 
 def find_path(dir_name: str = "data") -> Path:
+    """Convert string to Path and create necessary parent directories."""
     path = Path(dir_name)
     if not path.is_absolute():
         # Use the path of this file to find the root directory of the project
@@ -13,6 +14,18 @@ def find_path(dir_name: str = "data") -> Path:
 
 
 def _get_dataset(id: int, cache_dir: Union[str, Path]) -> openml.OpenMLDataset:
+    """Convenience function to fetch datasets from OpenML API.
+    Parameters
+    ----------
+    id : int
+        OpenML dataset id.
+    cache_dir : pathlib.Path or string
+        Path to local cache directory.
+
+    Returns
+    -------
+    OpenML dataset object.
+    """
     openml.config.apikey = "c3d6fbcd5a5741b5d4d4e15369a4f7fe"
     # OpenML can handle the caching of datasets
     openml.config.cache_directory = cache_dir
@@ -20,6 +33,7 @@ def _get_dataset(id: int, cache_dir: Union[str, Path]) -> openml.OpenMLDataset:
 
 
 def get_diabetes(data_dir: str = "data"):
+    """Get Diabetes dataset."""
     dir = find_path(data_dir)
     dataset = _get_dataset(42608, dir)
     df, _, _, _ = dataset.get_data(
@@ -28,6 +42,17 @@ def get_diabetes(data_dir: str = "data"):
     return df
 
 
+def get_spam(data_dir: str = "data"):
+    """Get spam dataset."""
+    dir = find_path(data_dir)
+    dataset = _get_dataset(44, dir)
+    df, _, _, _ = dataset.get_data(
+        target=dataset.default_target_attribute, dataset_format="dataframe"
+    )
+    return df
+
+
 if __name__ == "__main__":
-    print("Downloading all datasets...")
+    print("Downloading all datasets to cache...")
     get_diabetes()
+    get_spam()
--- experiments/scaling.py
+++ experiments/scaling.py
@@ -9,6 +9,7 @@ rng = np.random.default_rng(42)
 
 
 def measure_time(function):
+    """Measure CPU and wall times of a given function."""
     gc.collect()
     t1 = time.perf_counter(), time.process_time()
     function()
@@ -17,6 +18,7 @@ def measure_time(function):
 
 
 def bootstrap_data(df: pd.DataFrame, sample_size: int = 10_000) -> pd.DataFrame:
+    """Bootstrap data in a pandas dataframe."""
     N = len(df.index)
     idx = rng.integers(low=0, high=N, size=sample_size)
     return df.iloc[idx]
@@ -26,11 +28,32 @@ def run_tests(
     dataset: pd.DataFrame,
     tests: list[str],
     library: DSLibrary,
-    groupby_column: str,
-    sort_column: str,
+    groupby_column: str = None,
+    sort_column: str = None,
     n_repeats: int = 10,
     sample_size: int = 100_000,
 ) -> pd.DataFrame:
+    """Wrapper function to run whatever tests we want to perform.
+
+    Parameters
+    ----------
+    dataset : pd.DataFrame
+        Input data.
+    tests : list of strings
+        List of test names to be performed.
+    library : DSLibrary instance
+        Library to perform the tests with.
+    groupby_column : string, optional
+        Column name over which the groupby test is performed. If left empty, the test is
+        skipped.
+    sort_column : string, optional
+        Column name over which the sort test is performed. If left empty, the test is
+        skipped.
+    n_repeats : int, optional.
+        How many times tests are repeated (with different samples). Defaults to 10.
+    sample_size : int, optional.
+        Bootstrap sample size. Defaults to 100 000.
+    """
     res = np.zeros((n_repeats, len(tests)))
     for ti in range(n_repeats):
         print(f"Start test {ti+1}/{n_repeats}")
@@ -57,10 +80,10 @@ def run_tests(
 if __name__ == "__main__":
     df = get_diabetes()
     rdf = run_tests(
-        df,
-        ["drop_duplicates", "groupby", "sort"],
-        PDLibrary(),
-        "Pregnancies",
-        "Glucose",
+        dataset=df,
+        tests=["drop_duplicates", "groupby", "sort"],
+        library=PDLibrary(),
+        groupby_column="Pregnancies",
+        sort_column="Glucose",
     )
     print(rdf)
