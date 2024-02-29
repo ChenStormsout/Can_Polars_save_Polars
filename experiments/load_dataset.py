360c969593c4e363491f0c359669d4c1ff6d86ee
Korbinian Pfab <korbinian.pfab@siemens.com>
2024-02-29 18:59:58 +0200

    Fine tuning NYC dataset loading

--- experiments/load_dataset.py
+++ experiments/load_dataset.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import pandas as pd
-import cudf.pandas as cudf_pandas
+# import cudf.pandas as cudf_pandas
 import polars as pl
 
 
@@ -24,22 +24,22 @@ def load_parking_data_pandas() -> pd.DataFrame:
     return pd.read_parquet(file_path)
 
 
-def load_parking_data_cudf() -> cud_pandas.DataFrame:
-    """Util function that loads the NYC parking data into
-    a pandas DataFrame from 'nyc_parking_violations_2022.parquet'.
-    If the file does not exist, download_data.sh is executed to download
-    the file.
+# def load_parking_data_cudf() -> cudf_pandas.DataFrame:
+#     """Util function that loads the NYC parking data into
+#     a pandas DataFrame from 'nyc_parking_violations_2022.parquet'.
+#     If the file does not exist, download_data.sh is executed to download
+#     the file.
 
-    Returns
-    -------
-    pd.DataFrame
-        pandas DataFrame containing the NYC parking data from
-        'https://data.cityofnewyork.us/City-Government/Parking-Violations-Issued-Fiscal-Year-2022/7mxj-7a6y/about_data'
-    """
-    file_path = Path(__file__).parent.parent / "nyc_parking_violations_2022.parquet"
-    if not file_path.exists():
-        os.system("sh ./download_data.sh")
-    return cudf_pandas.read_parquet(file_path)
+#     Returns
+#     -------
+#     pd.DataFrame
+#         pandas DataFrame containing the NYC parking data from
+#         'https://data.cityofnewyork.us/City-Government/Parking-Violations-Issued-Fiscal-Year-2022/7mxj-7a6y/about_data'
+#     """
+#     file_path = Path(__file__).parent.parent / "nyc_parking_violations_2022.parquet"
+#     if not file_path.exists():
+#         os.system("sh ./download_data.sh")
+#     return cudf_pandas.read_parquet(file_path)
 
 
 def load_parking_data_polars() -> pl.DataFrame:
@@ -58,3 +58,12 @@ def load_parking_data_polars() -> pl.DataFrame:
     if not file_path.exists():
         os.system("sh ./download_data.sh")
     return pl.read_parquet(file_path)
+
+if __name__ == "__main__":
+    df=load_parking_data_polars()
+    # df.sort(by="")
+    print(df.columns)
+    print(df.describe())
+    df.sort(by="Plate ID")
+    df.drop()
+    print(len(df.columns))
